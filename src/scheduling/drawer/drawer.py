68904018b68fbe29890b5b166c82b0cc0f2a32f9
Marcelo Torres <marcelo.torres.o@hotmail.com>
2024-08-25 15:33:58 -0300

    drawing improvement

--- src/scheduling/drawer/drawer.py
+++ src/scheduling/drawer/drawer.py
@@ -25,10 +25,10 @@ class Drawer:
         self.ax.add_patch(rect)
 
     def add_green_energy_availability(self, start, width, power):
-        rect = patches.Rectangle((start, Drawer.BOUNDARY_HEIGHT), width, power, linewidth=1, edgecolor='green', facecolor='none')
+        rect = patches.Rectangle((start, 0), width, power, linewidth=1, edgecolor='green', facecolor='none')
         self.ax.add_patch(rect)
 
-    def add_scheduled_task(self, start, task, y, include_task_id=False):
+    def add_scheduled_task(self, start, task, y, include_task_id=True):
         edgecolor = (0, 0, 1, 0.9)
         facecolor = (0, 0, 1, 0.5)
 
@@ -39,6 +39,24 @@ class Drawer:
             cx = rx + rect.get_width() / 2.0
             cy = ry + rect.get_height() / 2.0
             self.ax.annotate(task.id, (cx, cy), color='black', weight='bold', fontsize=10, ha='center', va='center')
+
+        self.ax.add_patch(rect)
+
+    def add_line(self, x_list, y_list):
+        plt.plot(x_list, y_list, 'blue', lw=1)
+
+    def add_single_line(self, width, x, y):
+        plt.plot([x, x+width], [y, y], 'blue', lw=1)
+
+    def add_rectangle(self, width, height, x, y, description=None, edgecolor=(0, 0, 1, 0.9), facecolor=(0, 0, 1, 0.5)):
+
+        rect = patches.Rectangle((x, Drawer.BOUNDARY_HEIGHT + y), width, height, linewidth=1, edgecolor=edgecolor, facecolor=facecolor)
+
+        if description:
+            rx, ry = rect.get_xy()
+            cx = rx + rect.get_width() / 2.0
+            cy = ry + rect.get_height() / 2.0
+            self.ax.annotate(description, (cx, cy), color='black', weight='bold', fontsize=10, ha='center', va='center')
 
         self.ax.add_patch(rect)
 
