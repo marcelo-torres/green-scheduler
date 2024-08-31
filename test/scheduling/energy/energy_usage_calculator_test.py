3a155cc1c2193c96842bef03925650e6f7d39fe4
Marcelo Torres <marcelo.torres.o@hotmail.com>
2024-08-30 21:13:35 -0300

    test fix

--- test/scheduling/energy/energy_usage_calculator_test.py
+++ test/scheduling/energy/energy_usage_calculator_test.py
@@ -17,14 +17,14 @@ class EnergyUsageCalculatorTest(unittest.TestCase):
         green_energy = [630, 750, 900, 1050, 1200, 1380, 1200, 2010]
         interval_size = 300
         expected_green_power_available = [(0, 630), (300, 750), (600, 900), (900, 1050), (1200, 1200), (1500, 1380),
-                                          (1800, 1200), (2100, 2010)]
+                                          (1800, 1200), (2100, 2010), (2400, 0)]
         self.do_test_get_green_power_available(green_energy, interval_size, expected_green_power_available)
 
     def test_with_task_with_runtime_equal_to_interval_size(self):
         green_energy = [630, 750, 900, 1050, 1200, 1380, 1200, 2010]
         interval_size = 300
         expected_green_power_available = [(0, 630), (300, 750), (600, 900), (900, 1050), (1200, 1200-450), (1500, 1380),
-                                          (1800, 1200), (2100, 2010)]
+                                          (1800, 1200), (2100, 2010), (2400, 0)]
         scheduled_tasks = [
             (1200, Task(1, runtime=300, power=450))
         ]
@@ -35,7 +35,7 @@ class EnergyUsageCalculatorTest(unittest.TestCase):
         interval_size = 300
         expected_green_power_available = [(0, 630), (300, 750), (600, 900), (900, 1050), (1200, 1200 - 450),
                                           (1500, 1380-450), (1501, 1380),
-                                          (1800, 1200), (2100, 2010)]
+                                          (1800, 1200), (2100, 2010), (2400, 0)]
         scheduled_tasks = [
             (1200, Task(1, runtime=301, power=450))
         ]
@@ -46,7 +46,7 @@ class EnergyUsageCalculatorTest(unittest.TestCase):
         green_energy = [630, 750, 900, 1050, 1200, 1380, 1200, 2010]
         interval_size = 300
         expected_green_power_available = [(0, 630), (300, 750), (600, 900), (900, 1050), (1200, 1200-450-70),
-                                          (1500, 1380-70), (1501, 1380), (1800, 1200), (2100, 2010)]
+                                          (1500, 1380-70), (1501, 1380), (1800, 1200), (2100, 2010), (2400, 0)]
         scheduled_tasks = [
             (1200, Task(1, runtime=301, power=70)),
             (1200, Task(2, runtime=300, power=450))
@@ -64,7 +64,6 @@ class EnergyUsageCalculatorTest(unittest.TestCase):
                 calculator.add_scheduled_task(task, start_time)
 
         green_power_available = calculator.get_green_power_available()
-        print(green_power_available)
         self.assertEqual(green_power_available, expected_green_power_available)
 
 
