03a203251c0364f28f9642e5090055e233cda928
Marcelo Torres <marcelo.torres.o@hotmail.com>
2024-10-19 00:00:55 -0300

    Experiment execution refactored to multiprocessing

---
+++ src/experiments/ParallelExperimentExecutor.py
@@ -0,0 +1,41 @@
+import queue
+from multiprocessing import JoinableQueue, Process
+
+
+class ParallelExperimentExecutor:
+
+    def __init__(self, save_report):
+        self.save_report = save_report
+        self.reports_queue = None
+        self.report_handler = None
+        self.workers = []
+
+    def start(self):
+        self.reports_queue = JoinableQueue()
+        self.report_handler = Process(target=self._handle_report, daemon=True)
+        self.report_handler.start()
+
+    def run_experiment_async(self, experiment):
+        worker = Process(target=lambda: self._execute_experiment(experiment), daemon=True)
+        worker.start()
+        self.workers.append(worker)
+
+    def wait_all(self):
+        for worker in self.workers:
+            worker.join()
+        self.report_handler.kill()
+
+    def _handle_report(self):
+        while True:
+            try:
+                report = self.reports_queue.get(block=False)
+                self.save_report(report)
+                self.reports_queue.task_done()
+
+            except queue.Empty:
+                pass
+
+    def _execute_experiment(self, experiment):
+        report = experiment()
+        self.reports_queue.put(report)
+        self.reports_queue.join()
--- src/experiments/test/run_experiments.py
+++ src/experiments/test/run_experiments.py
@@ -1,10 +1,10 @@
 import csv
-from contextlib import redirect_stdout
 from datetime import datetime, timedelta
 import os
 
 from src.data.photovolta import PhotovoltaReader
 from src.data.wfcommons_reader import WfCommonsWorkflowReader
+from src.experiments.ParallelExperimentExecutor import ParallelExperimentExecutor
 from src.scheduling.algorithms.highest_power_first.highest_power_first import schedule_graph
 from src.scheduling.energy.energy_usage_calculator import EnergyUsageCalculator
 from src.scheduling.util.count_active_tasks import count_active_tasks
@@ -19,15 +19,16 @@ synthetic_path = f'{resources_path}/wfcommons/synthetic'
 MIN_TASK_POWER_DEFAULT = 1
 MAX_TASK_POWER_DEFAULT = 5
 
+
 def seconds_to_hours(seconds):
     return str(
         timedelta(seconds=seconds)
     )
 
-def report_scheduling(scheduling, graph, energy_calculator):
 
+def report_scheduling(scheduling, graph, energy_calculator):
     makespan = calc_makespan(scheduling, graph)
-    workflow_start_time = scheduling[graph.get_first_task().id] #TODO fix this
+    workflow_start_time = scheduling[graph.get_first_task().id]  #TODO fix this
     workflow_stretch = makespan - workflow_start_time
     print(f'\tMakespan: {makespan:,}s ({seconds_to_hours(makespan)})')
     print(f'\tWorkflow Stretch: {workflow_stretch:,}s ({seconds_to_hours(workflow_stretch)})')
@@ -37,7 +38,8 @@ def report_scheduling(scheduling, graph, energy_calculator):
     print(f'\tscheduling_hash: {scheduling_hash:}')
     print()
 
-    brown_energy_used, green_energy_not_used, total_energy = energy_calculator.calculate_energy_usage_for_scheduling(scheduling, graph)
+    brown_energy_used, green_energy_not_used, total_energy = energy_calculator.calculate_energy_usage_for_scheduling(
+        scheduling, graph)
     green_energy_used = total_energy - brown_energy_used
     print(f'\tBrown energy used: {brown_energy_used:,.{2}f}J')
     print(f'\t...Green energy used: {green_energy_used:,.{2}f}J')
@@ -65,20 +67,18 @@ def report_scheduling(scheduling, graph, energy_calculator):
     }
 
 
-def schedule_and_report(graph, green_power, interval_size, deadline_factor, task_ordering, shift_mode='right-left', c=0.0, show='off', id=0, total=0):
-
+def schedule_and_report(graph, green_power, interval_size, deadline_factor, task_ordering, shift_mode='right-left',
+                        c=0.0, show='off'):
     min_makespan = calc_critical_path_length(graph)
     deadline = min_makespan * deadline_factor
     number_of_tasks = len(graph.tasks)
 
-    print(f'Execution {id}/{total}')
-
     print(f'\tMin makespan: {min_makespan:,}s ({seconds_to_hours(min_makespan)})')
     print(f'\tDeadline: {deadline:,}s ({seconds_to_hours(deadline)})')
     print(f'\tNumber of tasks: {number_of_tasks:,.{2}f}')
-    print()
 
-    scheduling = schedule_graph(graph, deadline, green_power, interval_size, c=c, show=show, shift_mode=shift_mode, task_ordering=task_ordering)
+    scheduling = schedule_graph(graph, deadline, green_power, interval_size, c=c, show=show, shift_mode=shift_mode,
+                                task_ordering=task_ordering)
 
     energy_calculator = EnergyUsageCalculator(green_power, interval_size)
     scheduling_report = report_scheduling(scheduling, graph, energy_calculator)
@@ -97,13 +97,52 @@ def schedule_and_report(graph, green_power, interval_size, deadline_factor, task
 
     full_report = pre_scheduling_report | scheduling_report | post_scheduling_report
 
-    print(
-        'Violations: ', check(scheduling, graph)
-    )
+    print('\tViolations: ', check(scheduling, graph))
+    print()
 
     return full_report
 
 
+def experiments_per_workflow(experiment_parameters, metadata, parameters_report):
+
+    graph = experiment_parameters['graph']
+    green_power = experiment_parameters['green_power']
+    interval_size = experiment_parameters['interval_size']
+
+    shift_modes = experiment_parameters['shift_modes']
+    deadline_factors = experiment_parameters['deadline_factors']
+    c_values = experiment_parameters['c_values']
+    task_ordering_criterias = experiment_parameters['task_ordering_criterias']
+
+    prefix = metadata['prefix']
+    job_number = metadata['job_number']
+    job_count = metadata['job_count']
+    experiment_count = metadata['experiment_count']
+
+    reports = []
+    i = 1
+    for shift_mode in shift_modes:
+        for deadline_factor in deadline_factors:
+            for c_value in c_values:
+                for task_ordering in task_ordering_criterias:
+                    parameters_report_temp = parameters_report | {
+                        'experiment': f'J{job_number}_E{i}',
+                        'shift_mode': shift_mode,
+                        'deadline_factor': deadline_factor,
+                        'c_value': c_value,
+                        'task_ordering': task_ordering
+                    }
+
+                    print(f'JOB {job_number} of {job_count} | Experiment {i} of {experiment_count} | {prefix}')
+
+                    report = schedule_and_report(graph, green_power, interval_size, deadline_factor, task_ordering,
+                                                 c=c_value, shift_mode=shift_mode)
+
+                    full_report = parameters_report_temp | report
+                    reports.append(full_report)
+
+                    i += 1
+    return reports
 
 
 def create_dir(directory):
@@ -115,9 +154,53 @@ def get_experiment_id(dt):
     return dt.strftime("experiments_%Y-%m-%d_%H-%M-%S")
 
 
-def execute_experiments():
-    wfcommons_reader = WfCommonsWorkflowReader(synthetic_path)
+def write_reports_to_csv(reports, headers, file_full_path):
 
+    with open(file_full_path, 'a') as csvfile:
+        csvwriter = csv.writer(csvfile)
+
+        for report in reports:
+            row = []
+            for header in headers:
+                row.append(
+                    report[header]
+                )
+            csvwriter.writerow(row)
+
+
+def create_csv_file(start_time, headers):
+    experiment_id = get_experiment_id(start_time)
+    experiments_reports_path = resources_path + f'/experiments-shift/{experiment_id}'
+    file_full_path = f'{experiments_reports_path}/report_{experiment_id}.csv'
+
+    create_dir(experiments_reports_path)
+    with open(file_full_path, 'x') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(headers)
+
+    return file_full_path
+
+
+def execute_experiments():
+
+    headers = [
+        'experiment', 'workflow', 'energy_trace', 'shift_mode', 'c_value', 'deadline_factor', 'deadline',
+        'task_ordering', 'scheduling_hash',
+        'min_makespan', 'makespan', 'workflow_stretch',
+        'brown_energy_used', 'green_energy_used', 'total_energy', 'green_energy_not_used',
+        'max_active_tasks', 'active_tasks_mean', 'active_tasks_std', 'number_of_tasks',
+        'scheduling_violations',
+    ]
+
+    start_time = datetime.now()
+    file_full_path = create_csv_file(start_time, headers)
+
+    def save_report(report):
+        return write_reports_to_csv(report, headers, file_full_path)
+    executor = ParallelExperimentExecutor(save_report)
+
+
+    wfcommons_reader = WfCommonsWorkflowReader(synthetic_path)
     photovolta_reader = PhotovoltaReader(resources_path)
     interval_size = 300
 
@@ -133,86 +216,60 @@ def execute_experiments():
     ]
 
     green_power_providers = [
-        ('trace_1', lambda: photovolta_reader.get_trace_1(size=30)),
-        ('trace_2', lambda: photovolta_reader.get_trace_2(size=30))
+        ('trace-1', lambda: photovolta_reader.get_trace_1(size=30)),
+        ('trace-2', lambda: photovolta_reader.get_trace_2(size=30))
     ]
 
+    # Process parameters
     shift_modes = ['none', 'left', 'right-left']
     deadline_factors = [1, 2, 4, 8]
-
     c_values = [0, 0.2, 0.5, 0.8]
-
     task_ordering_criterias = ['energy', 'power', 'runtime']
 
-    experiments_count = len(workflow_providers) * len(green_power_providers) * len(shift_modes) * len(deadline_factors) * len(c_values) * len(task_ordering_criterias)
+    job_count = len(workflow_providers) * len(green_power_providers)
+    experiment_count = len(shift_modes) * len(deadline_factors) * len(c_values) * len(task_ordering_criterias)
 
-    headers = {
-        'experiment', 'workflow', 'energy_trace', 'shift_mode', 'deadline_factor', 'c_value', 'scheduling_hash',
-        
-        'makespan',
-        'workflow_stretch',
-        'brown_energy_used',
-        'green_energy_used',
-        'total_energy',
-        'green_energy_not_used',
-        'max_active_tasks',
-        'active_tasks_mean',
-        'active_tasks_std',
-        'min_makespan',
-        'deadline',
-        'number_of_tasks',
-        'task_ordering',
-        'scheduling_violations',
-    }
+    print(f'{experiment_count} experiments will be executed.')
 
-    start_time = datetime.now()
+    executor.start()
 
-    experiment_id = get_experiment_id(start_time)
-    experiments_reports_path = resources_path + f'/experiments-shift/{experiment_id}'
-    file_full_path = f'{experiments_reports_path}/report_{experiment_id}.csv'
-    log_file_full_path = f'{experiments_reports_path}/logs_{experiment_id}.txt'
+    j = 1
+    for workflow_name, workflow_provider in workflow_providers:
+        graph = workflow_provider()
 
-    create_dir(experiments_reports_path)
+        for g_trace_name, trace_provider in green_power_providers:
+            green_power = trace_provider()
 
-    with open(log_file_full_path, 'w') as f:
-        with redirect_stdout(f):
-            with open(file_full_path, 'x') as csvfile:
+            experiment_parameters = {
+                'graph': graph,
+                'green_power': green_power,
+                'interval_size': interval_size,
 
-                csvwriter = csv.writer(csvfile)
-                csvwriter.writerow(headers)
+                'shift_modes': shift_modes,
+                'deadline_factors': deadline_factors,
+                'c_values': c_values,
+                'task_ordering_criterias': task_ordering_criterias,
+            }
 
-                i = 1
-                for workflow_name, workflow_provider in workflow_providers:
-                    graph = workflow_provider()
+            metadata = {
+                'prefix': f'{workflow_name}_{g_trace_name}',
+                'job_number': j,
+                'job_count': job_count,
+                'experiment_count': experiment_count,
+            }
 
-                    for g_trace_name, trace_provider in green_power_providers:
-                        green_power = trace_provider()
+            parameters_report = {
+                'workflow': '' + workflow_name,
+                'energy_trace': '' + g_trace_name,
+            }
 
-                        for shift_mode in shift_modes:
-                            for deadline_factor in deadline_factors:
-                                for c_value in c_values:
-                                    for task_ordering in task_ordering_criterias:
-                                        parameters = {
-                                            'experiment': i,
-                                            'workflow': workflow_name,
-                                            'energy_trace': g_trace_name,
-                                            'shift_mode': shift_mode,
-                                            'deadline_factor': deadline_factor,
-                                            'c_value': c_value,
-                                            'task_ordering': task_ordering
-                                        }
+            executor.run_experiment_async(
+                lambda: experiments_per_workflow(experiment_parameters, metadata, parameters_report)
+            )
+            j += 1
 
-                                        report = schedule_and_report(graph, green_power, interval_size, deadline_factor, task_ordering, c=c_value, shift_mode=shift_mode, id=i, total=experiments_count)
+    executor.wait_all()
 
-                                        full_report = parameters | report
-
-                                        row = []
-                                        for header in headers:
-                                            row.append(
-                                                full_report[header]
-                                            )
-                                        csvwriter.writerow(row)
-                                        i += 1
     #import os
     #os.system("shutdown now -h")
 
@@ -240,10 +297,10 @@ def simple_execution():
     min_makespan = calc_critical_path_length(graph)
     print(f'\tMin makespan: {min_makespan:,}s ({seconds_to_hours(min_makespan)})')
 
-    schedule_and_report(graph, green_power, interval_size, deadline_factor, c=0.2, show='last', shift_mode='left', task_ordering='energy')
+    schedule_and_report(graph, green_power, interval_size, deadline_factor, c=0.2, show='last', shift_mode='left',
+                        task_ordering='energy')
 
 
-    
 if __name__ == '__main__':
     stopwatch = Stopwatch()
     stopwatch.start()
@@ -251,6 +308,4 @@ if __name__ == '__main__':
     #simple_execution()
     execute_experiments()
 
-    print(f'Overall execution: %.4fs' % stopwatch.get_elapsed_time()) # TODO format as datetime
-
-    pass
+    print(f'\n\nOverall execution: ', seconds_to_hours(stopwatch.get_elapsed_time()))
