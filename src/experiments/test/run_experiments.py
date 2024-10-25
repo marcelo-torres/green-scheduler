7735c805455e07792a81d00cde7f6d888a6cba5b
Marcelo Torres <marcelo.torres.o@hotmail.com>
2024-10-24 22:47:19 -0300

    print optimization

--- src/experiments/test/run_experiments.py
+++ src/experiments/test/run_experiments.py
@@ -54,32 +54,37 @@ def seconds_to_hours(seconds):
     )
 
 
-def report_scheduling(scheduling, graph, energy_calculator):
+def report_scheduling(scheduling, graph, energy_calculator, print_resport=False):
     makespan = calc_makespan(scheduling, graph)
 
     workflow_stretch = calc_stretch(graph, scheduling, makespan=makespan)
-    print(f'\tMakespan: {makespan:,}s ({seconds_to_hours(makespan)})')
-    print(f'\tWorkflow Stretch: {workflow_stretch:,}s ({seconds_to_hours(workflow_stretch)})')
-    print()
-
     scheduling_hash = hash(frozenset(scheduling.items()))
-    print(f'\tscheduling_hash: {scheduling_hash:}')
-    print()
 
     brown_energy_used, green_energy_not_used, total_energy = energy_calculator.calculate_energy_usage_for_scheduling(
         scheduling, graph)
     green_energy_used = total_energy - brown_energy_used
-    print(f'\tBrown energy used: {brown_energy_used:,.{2}f}J')
-    print(f'\t...Green energy used: {green_energy_used:,.{2}f}J')
-    print(f'\t...Total energy used: {total_energy:,.{2}f}J')
-    print(f'\tGreen energy not used: {green_energy_not_used:,.{2}f}J')
-    print()
 
     max_active_tasks, mean, std, active_tasks_by_time = count_active_tasks(scheduling, graph)
-    print(f'\tmax_active_tasks: {max_active_tasks:,.{0}f}')
-    print(f'\tactive_tasks_mean: {mean:,.{2}f}')
-    print(f'\tactive_tasks_std: {std:,.{2}f}')
-    print()
+
+    if print_resport:
+        print(f'\tMakespan: {makespan:,}s ({seconds_to_hours(makespan)})')
+        print(f'\tWorkflow Stretch: {workflow_stretch:,}s ({seconds_to_hours(workflow_stretch)})')
+        print()
+
+        print(f'\tscheduling_hash: {scheduling_hash:}')
+        print()
+
+        print(f'\tBrown energy used: {brown_energy_used:,.{2}f}J')
+        print(f'\t...Green energy used: {green_energy_used:,.{2}f}J')
+        print(f'\t...Total energy used: {total_energy:,.{2}f}J')
+        print(f'\tGreen energy not used: {green_energy_not_used:,.{2}f}J')
+        print()
+
+
+        print(f'\tmax_active_tasks: {max_active_tasks:,.{0}f}')
+        print(f'\tactive_tasks_mean: {mean:,.{2}f}')
+        print(f'\tactive_tasks_std: {std:,.{2}f}')
+        print()
 
     return {
         'makespan': makespan,
@@ -96,20 +101,21 @@ def report_scheduling(scheduling, graph, energy_calculator):
 
 
 def schedule_and_report(graph, green_power, interval_size, deadline_factor, task_ordering, shift_mode='right-left',
-                        c=0.0, show='off'):
+                        c=0.0, show='off', print_resport=False):
     min_makespan = calc_critical_path_length(graph)
     deadline = min_makespan * deadline_factor
     number_of_tasks = len(graph.tasks)
 
-    print(f'\tMin makespan: {min_makespan:,}s ({seconds_to_hours(min_makespan)})')
-    print(f'\tDeadline: {deadline:,}s ({seconds_to_hours(deadline)})')
-    print(f'\tNumber of tasks: {number_of_tasks:,.{2}f}')
+    if print_resport:
+        print(f'\tMin makespan: {min_makespan:,}s ({seconds_to_hours(min_makespan)})')
+        print(f'\tDeadline: {deadline:,}s ({seconds_to_hours(deadline)})')
+        print(f'\tNumber of tasks: {number_of_tasks:,.{2}f}')
 
     scheduling = schedule_graph(graph, deadline, green_power, interval_size, c=c, show=show, shift_mode=shift_mode,
                                 task_ordering=task_ordering)
 
     energy_calculator = EnergyUsageCalculator(green_power, interval_size)
-    scheduling_report = report_scheduling(scheduling, graph, energy_calculator)
+    scheduling_report = report_scheduling(scheduling, graph, energy_calculator, print_resport=print_resport)
 
     pre_scheduling_report = {
         'min_makespan': min_makespan,
@@ -125,8 +131,9 @@ def schedule_and_report(graph, green_power, interval_size, deadline_factor, task
 
     full_report = pre_scheduling_report | scheduling_report | post_scheduling_report
 
-    print('\tViolations: ', check(scheduling, graph))
-    print()
+    if print_resport:
+        print('\tViolations: ', check(scheduling, graph))
+        print()
 
     return full_report
 
@@ -333,18 +340,16 @@ def simple_execution():
     #graph = wfcommons_reader.read_genome_workflow(1000, 35.7812995246, random_gauss)
     #graph = wfcommons_reader.read_soykb_workflow(1000, 3.85224364443, random_gauss)
 
-    min_makespan = calc_critical_path_length(graph)
-    print(f'\tMin makespan: {min_makespan:,}s ({seconds_to_hours(min_makespan)})')
 
     schedule_and_report(graph, green_power, interval_size, deadline_factor, c=0.2, show='last', shift_mode='left',
-                        task_ordering='energy')
+                        task_ordering='energy', print_resport=True)
 
 
 if __name__ == '__main__':
     stopwatch = Stopwatch()
     stopwatch.start()
 
-    #simple_execution()
-    execute_experiments()
+    simple_execution()
+    #execute_experiments()
 
     print(f'\n\nOverall execution: ', seconds_to_hours(stopwatch.get_elapsed_time()))
