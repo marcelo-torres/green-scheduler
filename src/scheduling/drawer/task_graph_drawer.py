f1fb09806c15f52036d5b9ee3e36f23b7e65d4de
Marcelo Torres <marcelo.torres.o@hotmail.com>
2025-04-25 22:12:22 -0300

    graph drawer improvement

--- src/scheduling/drawer/task_graph_drawer.py
+++ src/scheduling/drawer/task_graph_drawer.py
@@ -5,28 +5,27 @@ import matplotlib
 
 matplotlib.use(matplotlib.get_backend()) #https://stackoverflow.com/a/52221178
 
-def draw_task_graph(graph, with_labels=False):
+def draw_task_graph(graph, with_labels=False, schedule=None, current_task_id=None):
 
-    G = _create_nx_graph(graph)
+    G, node_colors = _create_nx_graph(graph, schedule=schedule, current_task_id=current_task_id)
 
     for layer, nodes in enumerate(nx.topological_generations(G)):
         # `multipartite_layout` expects the layer as a node attribute, so add the
         # numeric layer value as a node attribute
         for node in nodes:
             G.nodes[node]["layer"] = layer
-            print(layer)
 
     # Compute the multipartite_layout using the "layer" node attribute
     pos = nx.multipartite_layout(G, subset_key="layer")
 
     fig, ax = plt.subplots()
-    nx.draw_networkx(G, pos=pos, ax=ax, with_labels=with_labels)
+    nx.draw_networkx(G, pos=pos, ax=ax, with_labels=with_labels, node_color=node_colors)
     ax.set_title("DAG layout in topological order")
     fig.tight_layout()
     plt.show()
 
 
-def _create_nx_graph(graph):
+def _create_nx_graph(graph, schedule=None, current_task_id=None):
 
     edges = []
 
@@ -37,9 +36,17 @@ def _create_nx_graph(graph):
             )
 
     G = nx.DiGraph(edges)
+    node_colors = []
 
     for task in graph.list_of_tasks():
         G.nodes[task.id]['runtime'] = task.runtime
         G.nodes[task.id]['power'] = task.power
 
-    return G
+        if task.id == current_task_id:
+            color = 'tab:orange' if schedule and task.id in schedule else 'tab:red'
+        else:
+            color = 'tab:green' if schedule and task.id in schedule else 'tab:blue'
+
+        node_colors.append(color)
+
+    return G, node_colors
