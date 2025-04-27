4e4f32f22916d6dcc9ad3f694aa3766707b7903a
Marcelo Torres <marcelo.torres.o@hotmail.com>
2025-04-27 18:49:33 -0300

    find mx start optimization

--- src/scheduling/util/find_start.py
+++ src/scheduling/util/find_start.py
@@ -21,16 +21,13 @@ def find_max_start_machine(task, machines, max_successor_start_time, start_limit
     max_machine = None
 
     for machine in machines:
-        start = max_successor_start_time - task.runtime
-        end = start + task.runtime
-
-        while start >= start_limit and not machine.can_schedule_task_in(task, start, end):
-            end = machine.state.previous_start(end) # TODO improve iteration strategy to be like find_min_start_machine
+        # TODO improve
+        for _, end in machine.search_intervals_to_schedule_task(task, start_limit, max_successor_start_time):
             start = end - task.runtime
 
-        if end > max_end and start >= start_limit:
-            max_end = end
-            max_machine = machine
+            if end > max_end and start >= start_limit:
+                max_end = end
+                max_machine = machine
 
     max_start = max_end - task.runtime
 
